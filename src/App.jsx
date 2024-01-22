eb7574f86d2c05dd7c1947dda213fe4541fa775a
tiago1820 <tiago.zdo@gmail.com>
2024-01-22 00:46:37 -0300

    Em portugues

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Esse é o contador {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
