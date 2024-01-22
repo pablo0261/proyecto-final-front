ce0f71dc4af1e50de8081f011f18960359224bd6
LinxInformatica <diegolepore01@gmail.com>
2024-01-22 00:31:54 -0300

    Pasar a español la leyenda count

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 5)}>
-          count is {count}
+          cuenta es {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
