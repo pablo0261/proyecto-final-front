403e2f2aca5287dc9082d415534c949cc039c8b6
nitidoart <113176158+nitidoart@users.noreply.github.com>
2024-01-23 16:30:35 +0100

    Primer commit de Landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom';
+import Landing from './views/landing/Landing';
+import Home from './views/home/Home';
+import NotFound from './utils/notFound/NotFound';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const nuevaVariable = "algo nuevo";
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>Hola!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 5)}>
-          cuenta es {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="textoAle">
-        Faccio un pasticcio - Prueba Ale
-      </p>
+      <Routes>
+        <Route exact path="/" element={<Landing/>}/>
+        <Route path="/home" element={<Home/>}/>
+
+        {/* Ruta para manejar rutas no definidas */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   )
 }
---
+++ src/views/landing/Landing.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+
+    return (
+        <div>
+
+            <Link to="/home">
+                <button>Home</button>
+            </Link>
+
+        </div>
+    );
+};
+
+export default Landing;
