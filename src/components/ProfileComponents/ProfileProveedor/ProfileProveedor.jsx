29d02a410da6751690d6b32d893857a0917182f3
pablo0261 <pablobesler@gmail.com>
2024-01-24 19:52:29 -0300

    Componente ProfileProveedor.jsx: configuración del JSON y estilos

--- src/components/ProfileComponents/ProfileProveedor/ProfileProveedor.jsx
+++ src/components/ProfileComponents/ProfileProveedor/ProfileProveedor.jsx
@@ -1,65 +1,73 @@
-import { useSelector} from "react-redux";
+import { useSelector } from "react-redux";
+import "./ProfileProveedor.style.css";
 
 function ProfileProveedor() {
-  
-  const infoDetailProveedor = useSelector((state) => state.infoDetailProveedor);
-  
-  const {
-    Verification,
-    imageId,
-    valoracion,
-    edad,
-    calle,
-    localidad,
-    provincia,
-    pais,
-    profesion,
-    descripcion,
-    telefono,
-    sobre,
-  } = infoDetailProveedor;
-  
-  const accountVerificationMessage = Verification
-  ? "Cuenta Verificada"
-  : "Cuenta no Verificada";
-  
- 
-  
+  const infoUserLog = useSelector((state) => state.infoUserLog);
+
+  //*AQUI LO QUE RECIBIRIA DEL BACK
+  const imageId = infoUserLog.image || "";
+  const valoracion = infoUserLog.valoration || "";
+  const age = infoUserLog.age || "";
+  const address = infoUserLog.address || "";
+  const idLocation = infoUserLog.idLocation || null;
+  const state = infoUserLog.state || "";
+  const area = infoUserLog.area || "";
+  const country = infoUserLog.country || "";
+  const profesion = infoUserLog.profesion || "";
+  const aboutMe = infoUserLog.aboutMe || "";
+  const phone = infoUserLog.phone || "";
+
+  const isAllInfoFilled =
+    imageId.length !== 0 &&
+    valoracion.length !== 0 &&
+    age.length !== 0 &&
+    address.length !== 0 &&
+    idLocation !== null &&
+    area.length !== 0 &&
+    state.length !== 0 &&
+    aboutMe.length !== 0 &&
+    country.length !== 0 &&
+    phone.length !== 0;
+
+  const Verification = isAllInfoFilled;
+
   return (
-    <div>
-      <div>{accountVerificationMessage}</div>
-      <div>
-        <div>
-          <img src={imageId} alt="Imagen" />
+    <div className="container">
+      <div className={Verification ? "verified" : "not-verified"}>
+        {Verification ? "Cuenta Verificada" : "Cuenta no Verificada"}
+      </div>
+      <div className="profile-container">
+        <div className="profile-info">
+          <img className="image" src={imageId} alt="Imagen" />
           <img src="Estrella" alt="Estrella" />
           <p>{valoracion}</p>
+          <p>{state}</p>
         </div>
         <div>
-          <div>
-            <h2>Claudia</h2>
-            <img src="ImagenVerificado" alt="Imagen Verificado" />
-            <p>{accountVerificationMessage}</p>
+          <div className="user-details">
+            <div className="user-header">
+              <h2 className="name">Claudia</h2>
+              <img src="ImagenVerificado" alt="Imagen Verificado" />
+              <p>
+                {Verification ? "Cuenta Verificada" : "Cuenta no Verificada"}
+              </p>
+            </div>
+            <p className="user-info">{`Edad: ${age}`}</p>
+            <p className="user-info">{`Dirección: ${address} ${area} ${country}`}</p>
           </div>
-          <p>
-            {edad}
-            {calle}
-            {localidad}
-            {provincia}
-            {pais}
-          </p>
-        </div>
-        <div>
-          <h3>{profesion}</h3>
-          <p>{descripcion}</p>
-          <div>
-            <p>Contactos :</p>
-            <img src={telefono} alt="Teléfono" />
-            <img src={sobre} alt="Sobre" />
+          <div className="profession-details">
+            <h3>{profesion}</h3>
+            <p>{aboutMe}</p>
+            <div className="contact-section">
+              <p>Contactos :</p>
+              <img src={phone} alt="Teléfono" />
+              <img src={"sobre"} alt="Sobre" />
+            </div>
           </div>
         </div>
+        <img src="editImage" alt="edit" className="edit-button" />
       </div>
-      <img  src="editImage" alt="edit" />
     </div>
   );
-  }
-  export default ProfileProveedor;
+}
+export default ProfileProveedor;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteSassPlugin from 'vite-plugin-sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    ViteSassPlugin(),
+    react()],
 })
