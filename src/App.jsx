09351e0a593e6976872803493dd276c2ef65ca08
Alejo Funes Abdala <alejofunes1@gmail.com>
2024-01-25 11:48:20 -0300

    App Routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NotFound from './utils/notFound/NotFound';
 
 import Helpers from './Helpers/RoutesFront';
 import AccessAccount from './Views/AccessAccount/AccessAccount';
-import ProfileProveedorView from './views/ProfileProveedorView/PerfilProveedorView';
+import ProfileProveedor from './components/ProfileComponents/ProfileProveedor/profileproveedor';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
@@ -16,17 +16,13 @@ function App() {
       <NavBar></NavBar>
       <Routes>
         <Route exact path={Helpers.Landing} element={<Landing/>}/>
-        <Route exact path="/" element={<Landing/>}/>
-        <Route path="/home" element={<Home/>}/>
-
-        {/* Ruta para manejar rutas no definidas */}
         <Route path={Helpers.AccessAccount} element={<AccessAccount/>} />
 
         {/* Cliente */}
         <Route path={Helpers.HomeCustomer} element={<Home/>}/>
 
         {/* Proveedor */}
-        <Route path={Helpers.ProfileProveedorView} element={<ProfileProveedorView/>}/>
+        <Route path={Helpers.ProfileProveedorView} element={<ProfileProveedor/>}/>
 
         {/* Administrador */}
 
