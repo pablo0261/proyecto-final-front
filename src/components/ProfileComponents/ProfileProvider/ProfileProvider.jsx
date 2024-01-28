d76c2b7b902d6f286a29b6b808f172725be7157d
Alejo Funes Abdala <alejofunes1@gmail.com>
2024-01-27 22:34:43 -0300

    ProfileProvidor_Datospersonales : SASS Completo

--- src/components/ProfileComponents/ProfileProvider/ProfileProvider.jsx
+++ src/components/ProfileComponents/ProfileProvider/ProfileProvider.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import style from './ProfileProvider.module.sass';
+import Helpers from "../../../Helpers/RoutesFront";
+import defaultImage from '../../../assets/image/PerfilImage.png';
 
 function ProfileProvider() {
   const infoUserLog = useSelector((state) => state.infoUserLog);
@@ -23,45 +25,48 @@ function ProfileProvider() {
     (value) => value.length !== 1
   );
 
-  const Verification = isAllInfoFilled;
+  const Verification = isAllInfoFilled
+  const component = 1
 
   return (
     <div className={style.background}>
       <div className={style.wrapper}>
-        {
-          Verification /* && isProvider */ &&
-          <div className={style.verificationAlert}>
-            <p className={style.textAlert}>Completa tu perfil para poder verificar tu cuenta. Ten en cuenta que los perfiles no verificados
-              no son mostrados a los clientes.</p>
-          </div>
-        }
+        <div className={style.alertWrapper}>
+          {
+            !Verification /* && isProvider */ &&
+            <div className={style.verificationAlert}>
+              <p className={style.textAlert}>Completa tu perfil para poder verificar tu cuenta. Ten en cuenta que los perfiles no verificados
+                no son mostrados a los clientes.</p>
+            </div>
+          }
+        </div>
         <div className={style.perfilWrapper}>
           <div className={style.imageWrapper}>
             <div className={formData.state === 'Active' ? style.stateActive : style.stateInactive}>{formData.state}</div>
-            <img className={style.image} src={formData.imageId} alt="Imagen" />
+            <img className={style.image} src={defaultImage} alt="Imagen"/>
             <div className={style.valoration}>
-              <div className={style.star}>Star</div>
-              <p className={style.imageWrapper}>({formData.valoracion}5)</p>
+              <div className={style.starIcon}></div>
+              <p className={style.textStar}>4.5 (40)</p>
             </div>
           </div>
-          <div>
-            <div className="user-details">
-              <p className={style.name}>Claudia</p>
-              <div className="verificacion">IconoVerificacion</div>
+          <div className={style.infoWrapper}>
+            <div className={style.nameWrapper}>
+              <p className={style.textName}>Claudia</p>
+              <div className={Verification ? style.iconVerified : style.iconNotVerified}></div>
               {
-                Verification ? "Cuenta Verificada" : "Cuenta no Verificada"
+                Verification ? <p className={style.textVerified}>Cuenta Verificada</p> : <p className={style.textNotVerified}>Cuenta No Verificada</p>
               }
-              <Link to={{ pathname: `/form/${1}` }} className="edit">Editar</Link>
+              <Link to={Helpers.Form.replace(':component', component)} className={style.editButton}></Link>
             </div>
-            <p className="user-info">{`${formData.age} años | ${formData.address}, ${formData.area}, ${formData.country}`}</p>
-            <p>{formData.profesion}</p>
-            <p>{formData.aboutMe}</p>
-            <div className="contact-section">
-              <p>Contactos :</p>
-              <div className="telefono">Tel</div>
-              <p>{formData.phone}</p>
-              <div className="email">Email</div>
-              <p>{formData.email}</p>
+            <p className={style.textData}>{`${formData.age} años | ${formData.address}, ${formData.area}, ${formData.country}`}</p>
+            <p className={style.textOcupation}>{formData.profesion}</p>
+            <p className={style.textDetail}>{formData.aboutMe}</p>
+            <div className={style.contacts}>
+              <p className={style.textContact}>Contactos:</p>
+              <div className={style.iconEmail}></div>
+              { /* isProvider && */ <p className={style.textEmail}>claudia@email.com</p> }
+              <div className={style.iconPhone}></div>
+              { /* isProvider && */ <p className={style.textPhone}>+54 380 4 785215</p> }
             </div>
           </div>
         </div>
