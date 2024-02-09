21cb5cb71a48b5ddda7964ccfa35f2c4ec192386
pablo0261 <pablobesler@gmail.com>
2024-02-08 23:50:28 -0300

    mejoras en sass de education

--- src/components/ProfileComponents/EducationProvider/EducationProvider.jsx
+++ src/components/ProfileComponents/EducationProvider/EducationProvider.jsx
@@ -19,19 +19,16 @@ function EducationProvider() {
       infoUserLog.categories &&
       infoUserLog.categories.length > 0
     ) {
-      const educacion = infoUserLog.categories[1].description;
       const educationOptions = infoUserLog.categories[1].categories_options;
 
       if (educationOptions && educationOptions.length > 0) {
         const educationData = educationOptions.map((option) => ({
           idOption: option.idOption,
-          education: educationOptions[0].description || "No description",
-          institution:
-            educationOptions[0].people_options[0].institution || "Escuela N-7",
-          year: educationOptions[0].people_options[0]?.year || "2004",
+          education: option.description || "No description",
+          institution: option.people_options[0].institution || "Escuela N-7",
+          year: option.people_options[0]?.year || "2004",
           observaciones:
-            educationOptions[0].people_options[0]?.comment ||
-            "orientacion técnica",
+            option.people_options[0]?.comment || "orientacion técnica",
         }));
 
         setEducation(educationData);
@@ -39,46 +36,43 @@ function EducationProvider() {
     }
   }, []);
 
+  const handleDeleteService = () => {
+    dispatchEvent(deleteEducation(education));
+  };
+
   return (
     <div className={style.container}>
       <div className={style.titleContainer}>
         <h1 className={style.title}>Educación</h1>
-        <button
-          onClick={() => handleShowForm()}
-          className={style.editButton}
-        ></button>
+        <button onClick={handleShowForm} className={style.editButton}></button>
       </div>
 
-      <div >
+      <div className={style.educationdetailContainer}>
         {education.map((option) => (
-          <div className={style.educationdetailContainer} key={option.idOption}>
-            <div className={style.infoContainerLeft}>
-              <h2 className={style.education}>{option.education}</h2>
-              <p className={style.detailInfo}>
-                {option.institution}
-                <br />
-                {option.year}
-              </p>
-            </div>
-
-            <div className={style.infoContainerRight}>
-              <p className={style.observationInfo} >
-                Observaciones: {option.observaciones}
-              </p>
-            </div>
-
-            <button
-              onClick={() => handleDeleteService()}
-              className={style.crossButton}
-            ></button>
-
-            {/* {option.nivelesDeEducacion.map((nivel) => (
-              <div key={nivel.id}>
-                <h3>{nivel.titulo}</h3>
-                <p>Descripción: {nivel.descripcion}</p>
+          <div key={option.idOption} >
+            <div className={style.educationdetailbox}>
+              <div className={style.infoContainerLeft}>
+                <h2 className={style.education}>{option.education}</h2>
+                <p className={style.detailInfo}>
+                  {option.institution}
+                  <br />
+                  {option.year}
+                </p>
               </div>
-            ))} */}
-            {showForm && <Form handleShowForm={handleShowForm} />}
+              <div className={style.infoContainerRight}>
+                <p className={style.observationInfo}>
+                  Observaciones: {option.observaciones}
+                </p>
+              </div>
+              <button
+                onClick={handleDeleteService}
+                className={style.crossButton}
+              ></button>
+              {showForm && <Form handleShowForm={handleShowForm} />}
+            </div>
+            <div>
+              <p className={style.line}></p>
+            </div>
           </div>
         ))}
       </div>
