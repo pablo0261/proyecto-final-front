ad702cd9305bc13872a76e054ff81b233c598f4e
Alejo Funes Abdala <alejofunes1@gmail.com>
2024-02-10 05:41:11 -0300

    Complejizo el CHAT con renderizados condicionales. Cancelar el Servicio esta disponible en la ventana de confirmar. Arreglo otros bugs de otros componentes. Creo componentes para diferneciar la responsabilidad si puede o no chatear

--- src/components/Connections/ChatBox/ChatBox.jsx
+++ src/components/Connections/ChatBox/ChatBox.jsx
@@ -3,31 +3,34 @@ import style from './ChatBox.module.sass'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import ChatRender from './ChatRender/ChatRender'
+import { putOpportunities } from '../../../redux/actions'
 
 function ChatBox(props) {
 
-    const { idOpportunitie, infoUserLog } = props
+    const { idOpportunitie, infoUserLog, opportunities } = props
     const [dataChat, setDataChat] = useState([])
     const [isLoadingChat, setIsLoadingChat] = useState()
-   
+
     const REACT_APP_API_URL = import.meta.env.VITE_BASE_URL
 
-    useEffect(()=>{
-        const setChat = async () => {
-            try {
-                setIsLoadingChat(true)
-                const response = await axios.get(`${REACT_APP_API_URL}/chats?idOpportunitie=${idOpportunitie}&idPeople=${infoUserLog.idPeople}`)
-                if (response.status === 200) {
-                  setDataChat(response.data.data)
-                  setIsLoadingChat(false)
-                }
-              } catch (error) {
-                window.alert(error)
-              }
+    const setChat = async () => {
+        try {
+            setIsLoadingChat(true)
+            const response = await axios.get(`${REACT_APP_API_URL}/chats?idOpportunitie=${idOpportunitie}&idPeople=${infoUserLog.idPeople}`)
+            if (response.status === 200) {
+                setDataChat(response.data.data)
+                setIsLoadingChat(false)
+            }
+        } catch (error) {
+            window.alert(error)
         }
+    }
 
-        setChat()
-    },[idOpportunitie])
+    useEffect(() => {
+        if (idOpportunitie.length > 0) {
+            setChat()
+        }
+    }, [idOpportunitie])
 
     const [message, setMessage] = useState({
         idOpportunitie: idOpportunitie,
@@ -45,7 +48,7 @@ function ChatBox(props) {
             axios.post(`${REACT_APP_API_URL}/chats`, message)
                 .then((response) => {
                     if (response.status === 200) {
-                        setMessage({...message, message: ""})
+                        setMessage({ ...message, message: "" })
                     }
                 })
                 .catch((reason) => window.alert(reason))
@@ -54,29 +57,79 @@ function ChatBox(props) {
         }
     }
 
+    const [showForm, setShowForm] = useState(false)
+    const [cancelation, setCancelation] = useState({
+        idOpportunitie: idOpportunitie,
+        idPeople: infoUserLog.idPeople,
+        cancelled: true,
+        reasonForCancelation: ""
+    })
+
+    const handleShowInput = () => {
+        setShowForm(!showForm)
+        setCancelation({ ...cancelation, reasonForCancelation: "" })
+    }
+
+    const handleChangeStateForm = (event) => {
+        setCancelation({ ...cancelation, reasonForCancelation: event.target.value })
+    }
+
+    const dispatch = useDispatch()
+    const handleChangeState = (event) => {
+        event.preventDefault()
+        if (!Object.values(cancelation).every((property) => property === "")) {
+            dispatch(putOpportunities(cancelation))
+        } else {
+            window.alert("Debes indicar el porque")
+        }
+    }
+
     return (
         <div className={style.background}>
             {
-                !isLoadingChat && dataChat.length != 0
-                    ? <div className={style.chatWrapper}>
-                        <div className={style.msgWrapper}>
-                            <ChatRender dataChat={dataChat}></ChatRender>
+                opportunities.length != 0
+                    ? !isLoadingChat && dataChat.length != 0
+                        ? <div className={style.chatWrapper}>
+                            <div className={style.buttonWrapper}>
+                                <div className={style.buttonProcess} onClick={() => handleShowInput()}> Cancelar Contrato </div>
+                                {
+                                    showForm &&
+                                    <form className={style.formCancel} onSubmit={handleChangeState}>
+                                        <input
+                                            type='text'
+                                            name='reasonForCancelation'
+                                            value={cancelation.reasonForCancelation}
+                                            onChange={handleChangeStateForm}
+                                            placeholder='¿Por que quieres cancelar el contrato?'
+                                            className={style.inputCancel}
+                                        ></input>
+                                        <button type='submit' className={style.buttonSubmit}>Confirmar</button>
+                                    </form>
+                                }
+                                {
+                                    infoUserLog.typeOfPerson === 'provider' && <div className={style.buttonProcess} onClick={() => handleChangeState()}> Contrato Finalizado </div>
+                                }
+                            </div>
+                            <div className={style.msgWrapper}>
+                                <ChatRender dataChat={dataChat}></ChatRender>
+                            </div>
+                            <div className={style.inputWrapper}>
+                                <form onSubmit={handleSendChat} className={style.formChat}>
+                                    <input
+                                        type='text'
+                                        name='chat'
+                                        value={message.message}
+                                        placeholder='Escribe un mensaje'
+                                        onChange={handleChangeMessage}
+                                        className={style.inputChat} />
+                                    <button type='submit' className={style.buttonChat}>Enviar</button>
+                                </form>
+                            </div>
                         </div>
-                        <div className={style.inputWrapper}>
-                            <form onSubmit={handleSendChat} className={style.formChat}>
-                                <input
-                                    type='text'
-                                    name='chat'
-                                    value={message.message}
-                                    placeholder='Escribe un mensaje'
-                                    onChange={handleChangeMessage}
-                                    className={style.inputChat} />
-                                <button type='submit' className={style.buttonChat}>Enviar</button>
-                            </form>
-                        </div>
-                    </div>
+                        :
+                        <p className={style.loadingChat}>Cargando Chat</p>
                     :
-                    <p className={style.loadingChat}>Cargando chat</p>
+                    <p className={style.loadingChat}>No tienes contactos confirmados</p>
             }
         </div>
     )
