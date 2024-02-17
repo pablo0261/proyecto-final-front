7b1c5170c4b707607b34a7b7ea6298e0798b2969
leandroh1002 <leandroherrera1002@gmail.com>
2024-02-17 02:53:00 -0300

    delete import

--- src/components/TableUser/TableUser.jsx
+++ src/components/TableUser/TableUser.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from "./TableUser.module.scss";
 import { useDispatch } from 'react-redux';
 import { putState } from "../../redux/actions";
-import styles from "./TableUser.module.sass";
 
 function TableDue({people}) {
   const dispatch = useDispatch();
