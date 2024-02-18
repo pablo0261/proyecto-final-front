bfb5c5ca097db5fd64af7f28d65196e4a104aac8
leandroh1002 <leandroherrera1002@gmail.com>
2024-02-18 18:21:47 -0300

    agregado de botones de filtros y direccionamiento al mail

--- src/components/TableUserDue/TableUserDue.jsx
+++ src/components/TableUserDue/TableUserDue.jsx
@@ -1,25 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import styles from "./TableUserDue.module.scss";
 import Pagination from '../Pagination/Pagination';
-import { allProviderAdmin } from '../../redux/actions';
+import { allProviderAdmin, getPeopleFilteredOrderedPagination, saveSelectionsGlobal } from '../../redux/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import Form from "../Form/FormMail/FormMail"
 
 function TableUserDue() {
   const REACT_APP_API_URL = import.meta.env.VITE_BASE_URL;
+  const filterOrderSelectedGlobal = useSelector((state) => state.filterOrderSelected);
   const people = useSelector((state) => state.providerForAdmin.data);
   const InfoPag = useSelector((state) => state.providerForAdmin);
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [showForm, setShowForm] = useState(false);
+  const [showFilters, setShowFilters] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
+  //! desde aqui esta el filtro
+  const [selectedOrder, setSelectedOrder] = useState(filterOrderSelectedGlobal.orders);
+  const [selectedServices, setSelectedServices] = useState(filterOrderSelectedGlobal.filters);
+  const [isLoading, setIsLoading] = useState()
+  const [queryProps, setQueryProps] = useState("")
   const handleShowForm = () => {
     setShowForm(!showForm);
   };
 
   useEffect(() => {
-    dispatch(allProviderAdmin(""));
-  }, []);
+    const handleLoading = async () => {
+      try {
+        if (Object.values(filterOrderSelectedGlobal).every(property => property.length === 0)) {
+          setIsLoading(true);
+          await dispatch(getPeopleFilteredOrderedPagination(""));
+          setIsLoading(false);
+        } else {
+          // Si la validación no se cumple, realizar el dispatch con otro valor
+          await dispatch(allProviderAdmin("")); // Reemplaza "otro-valor" con el valor deseado
+        }
+      } catch (error) {
+        // Manejar el error si ocurre durante la carga
+        console.error("Error durante la carga:", error);
+        setIsLoading(false);
+      }
+    };
+  
+    handleLoading();
+  }, [dispatch, filterOrderSelectedGlobal]);
+  
 
   const handleChangeStatus = async (value, state) => {
     const auxState = state === "Active" ? "Inactive" : "Active";
@@ -34,21 +60,155 @@ function TableUserDue() {
 
   const handlerPagination = (queryConstructOrder) => {
     dispatch(allProviderAdmin(queryConstructOrder));
+    dispatch(getPeopleFilteredOrderedPagination(queryProps, queryConstructOrder));
   };
-
   if (!people) {
     return null;
   }
   const onMailButtonClick = (email) => {
     setEmail(email)
   }
+  
+
+//! aqui van las funciones del filtro
+  const handleFilterVisibility = () => {
+    setShowFilters(!showFilters);
+    if (!showFilters) {
+      setShowOrder(false);
+    }
+  };
+
+  const handleOrderVisibility = () => {
+    setShowOrder(!showOrder);
+    if (!showOrder) {
+      setShowFilters(false);
+    }
+  };
+
+  const handleOrderSelected = (order) => {
+    if (selectedOrder.includes(order)) {
+      setSelectedOrder(selectedOrder.filter((selected) => selected !== order));
+    } else {
+      let updatedOrder;
+      if (order.includes('fullName,')) {
+        updatedOrder = selectedOrder.filter((selected) => !selected.includes('fullName,'));
+      } else if (order.includes('averageRating,')) {
+        updatedOrder = selectedOrder.filter((selected) => !selected.includes('averageRating,'));
+      }
+      setSelectedOrder([...updatedOrder, order]);
+    }
+  };
+
+  const handleApply = async () => {
+    setIsLoading(true)
+    setShowOrder(false);
+    setShowFilters(false);
+    dispatch(saveSelectionsGlobal({ filters: selectedServices, orders: selectedOrder }));
+    const query = `&idOption=${selectedServices.map((idOption) => idOption).join()}&idOrder=${selectedOrder.map((idOrder) => idOrder).join(";")}`
+    setQueryProps(query)
+    await dispatch(getPeopleFilteredOrderedPagination(query));
+    setIsLoading(false)
+  };
+  const handleServiceSelected = (service) => {
+    setSelectedServices((prevSelectedServices) => {
+      if (prevSelectedServices.includes(service)) {
+        return prevSelectedServices.filter((selectedService) => selectedService !== service);
+      } else {
+        return [...prevSelectedServices, service];
+      }
+    });
+  };
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.container_header}>
         <h2>Usuarios en deuda</h2>
+        <a href="http://www.diegolepore.com.ar/webmail" target="_blank"><button>Ir a Webmail</button></a>
+        <div className={styles.filterOrderContainer}>
+            <button className={showFilters ? styles.buttonActived : styles.button} onClick={() => handleFilterVisibility()}>Filtrar</button>
+            <button className={showOrder ? styles.buttonActived : styles.button} onClick={() => handleOrderVisibility()}>Ordenar</button>
+          </div>
       </div>
-      
+      <div className={styles.filterOrderBox}>
+            {showFilters && (
+              <div className={styles.Box}>
+              <div className={styles.filterBox}>
+                <p className={styles.titleCategory}>Deudor</p>
+                <div className={styles.optionsWrapper}>
+                  <button
+                    onClick={() => handleOrderSelected('price,DESC')}
+                    className={selectedOrder.includes('price,DESC') ? 'selected' && styles.optionSelected : styles.optionNotSelected}
+                  >
+                    A-Z
+                  </button>
+                  <button
+                    onClick={() => handleOrderSelected('price,ASC')}
+                    className={selectedOrder.includes('price,ASC') ? 'selected' && styles.optionSelected : styles.optionNotSelected}
+                  >
+                    Z-A
+                  </button>
+                </div>
+                <p className={styles.titleCategory}>A Vencer </p>
+                <div className={styles.optionsWrapper}>
+                  <button
+                    onClick={() => handleOrderSelected('averageRating,DESC')}
+                    className={selectedOrder.includes('averageRating,DESC') ? 'selected' && styles.optionSelected : styles.optionNotSelected}
+                  >
+                    Deuda Reciente
+                  </button>
+                  <button
+                    onClick={() => handleOrderSelected('averageRating,ASC')}
+                    className={selectedOrder.includes('averageRating,ASC') ? 'selected' && styles.optionSelected : styles.optionNotSelected}
+                  >
+                    Deuda antigua
+                  </button>
+                </div>
+              </div>
+              <div className={styles.applyWrapper}>
+                <button className={styles.button} onClick={handleApply}>Aplicar</button>
+              </div>
+            </div>
+            )}
+            {showOrder && (
+              <div className={styles.Box}>
+                <div className={styles.filterBox}>
+                  <p className={styles.titleCategory}>Nombre</p>
+                  <div className={styles.optionsWrapper}>
+                    <button
+                      onClick={() => handleOrderSelected('fullName,DESC')}
+                      className={selectedOrder.includes('fullName,DESC') ? 'selected' && styles.optionSelected : styles.optionNotSelected}
+                    >
+                      A-Z
+                    </button>
+                    <button
+                      onClick={() => handleOrderSelected('fullName,ASC')}
+                      className={selectedOrder.includes('fullName,ASC') ? 'selected' && styles.optionSelected : styles.optionNotSelected}
+                    >
+                      Z-A
+                    </button>
+                  </div>
+                  <p className={styles.titleCategory}>Fecha Deuda</p>
+                  <div className={styles.optionsWrapper}>
+                    <button
+                      onClick={() => handleOrderSelected('averageRating,DESC')}
+                      className={selectedOrder.includes('averageRating,DESC') ? 'selected' && styles.optionSelected : styles.optionNotSelected}
+                    >
+                      Deuda Reciente
+                    </button>
+                    <button
+                      onClick={() => handleOrderSelected('averageRating,ASC')}
+                      className={selectedOrder.includes('averageRating,ASC') ? 'selected' && styles.optionSelected : styles.optionNotSelected}
+                    >
+                      Deuda antigua
+                    </button>
+                  </div>
+                </div>
+                <div className={styles.applyWrapper}>
+                  <button className={styles.button} onClick={handleApply}>Aplicar</button>
+                </div>
+              </div>
+            )}
+          </div>
 
       <table>
         <thead>
@@ -85,11 +245,8 @@ function TableUserDue() {
       {showForm && <Form handleShowForm={handleShowForm} email={email}/>}
 
       <div>
-      <Pagination pageNumber={InfoPag.pageNumber} totalOfPages={InfoPag.totalOfPages} onPageChange={handlerPagination}/>
+      <Pagination pageNumber={InfoPag.pageNumber} totalOfPages={InfoPag.totalOfPages} onPageChange={handlerPagination} queryProps={queryProps}/>
       </div>
-
-
-
     </div>
   );
 }
