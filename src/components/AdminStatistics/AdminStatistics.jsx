3bb23865faffa77f88d11e875bfab9a566518a24
Alejo Funes Abdala <alejofunes1@gmail.com>
2024-02-19 16:56:45 -0300

    admin

--- src/components/AdminStatistics/AdminStatistics.jsx
+++ src/components/AdminStatistics/AdminStatistics.jsx
@@ -7,39 +7,24 @@ function AdminStatistics() {
   return (
     <div className={style.background}>
       <div className={style.dashboardWrapper}>
-        <div className={style.containerLeft}>
-          <p className={style.title}>Servicios mas buscados</p>
-          <div className={style.divMasBuscados}>
-            <div className={style.cardMasBuscados}>
-              <MostSearch />
-            </div>
-          </div>
-          <div className={style.divPersonasQueTeBuscaron}>
-            <DayFlow />
-          </div>
-          <h2 className={style.h2HistoriaServ}>Grafico 1</h2>
-          <div className={style.divHistorialpagos}>
-            <div></div>
-          </div>
-          <h2 className={style.h2HistoriaServ}>Grafico 2</h2>
-          <div className={style.divHistorialpagos}>
-            <div></div>
-          </div>
+        {/* SERVICIOS MAS BUSCADOS */}
+        <div className={style.container}>
+          <p className={style.mostSearch}>Servicios mas buscados</p>
+          <MostSearch />
         </div>
-
-        <div className={style.containerRight}>
-          <p className={style.title}>Grilla 2 x 3 de valores generales</p>
-          <div className={style.divPromedioDeCalific}>
-            <ValoresGenerales />
-          </div>
-          <h2 className={style.h2HistoriaServ}>Grafico 3</h2>
-          <div className={style.divHistorialpagos}>
-            <div></div>
-          </div>
-          <h2 className={style.h2HistoriaServ}>Grafico 4</h2>
-          <div className={style.divHistorialpagos}>
-            <div></div>
-          </div>
+        {/* NUMEROS  */}
+        <div className={style.container}>
+          <ValoresGenerales />
+        </div>
+        {/* GRAFICO 1  */}
+        <div className={style.containerG}>
+          <h2 className={style.h2HistoriaServ}>Grafico 1</h2>
+          {/* Aqui va el componente del Grafico 1 */}
+        </div>
+        {/* GRAFICO 2  */}
+        <div className={style.containerG}>
+          <h2 className={style.h2HistoriaServ}>Grafico 2</h2>
+          {/* Aqui va el componente del Grafico 2 */}
         </div>
       </div>
     </div>
