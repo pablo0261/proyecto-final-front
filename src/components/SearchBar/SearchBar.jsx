959bc4c582ede755c29e97b4255543563c6f4e0b
Alejo Funes Abdala <alejofunes1@gmail.com>
2024-02-21 01:41:32 -0300

    Terminadas las Tablas de Usuarios para ADMIN _ Corrigo age del SignIn

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,38 +1,45 @@
 import React, { useState } from "react";
-import { clear } from "../../redux/actions";
+import { allPeople, clear } from "../../redux/actions";
 import { useDispatch } from "react-redux";
 import styles from "./SearchBar.module.scss";
 
-export default function SearchBar({onSearchChange}) {
-  const [searchInput, setSearchInput] = useState("");
+export default function SearchBar(props) {
+
+  const {searchInput, setSearchInput} = props
   const dispatch = useDispatch();
 
-  let algunvalor = "";
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (searchInput.length != 0) {
+      const query = `&fullName=${searchInput}`
+      dispatch(allPeople(query))
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearchInput(event.target.value);
+  }
 
   const handleClear = () => {
     setSearchInput("")
     dispatch(clear())
   }
 
-  const handleChange = (event) => {
-    setSearchInput(event.target.value);
+  const handleAll = () => {
+    dispatch(allPeople(""))
   }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (searchInput !== "") {
-      algunvalor = "&fullName=" + searchInput;
-    }
-    onSearchChange(algunvalor);
-  };
 
   return (
     <div className={styles.container}>
-
       <form className={styles.container_form} onSubmit={handleSubmit}>
-        <input type="search" placeholder="Buscar" onChange={handleChange} value={searchInput} />
+        <input
+          type="search"
+          placeholder="Buscar usuario por Nombre Completo"
+          onChange={handleChange}
+          value={searchInput} />
         <button type="submit">Buscar</button>
-        <button type="button" onClick={handleClear}>Limpiar</button>
+        <button type="button" onClick={()=>handleClear()}>Limpiar</button>
+        <button type="button" onClick={()=>handleAll()}>Todos</button>
       </form>
 
     </div>
