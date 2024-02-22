421de32e60d64d480a7d096ebaf88258974eb82a
Alejo Funes Abdala <alejofunes1@gmail.com>
2024-02-22 11:32:04 -0300

    Profile Provider, arreglo de errores

--- src/components/ProfileComponents/ProfileProvider/ProfileProvider.jsx
+++ src/components/ProfileComponents/ProfileProvider/ProfileProvider.jsx
@@ -20,7 +20,7 @@ function ProfileProvider() {
 
   const handleChangeStatus = (status) => {
     const newStatus = {
-      state: status, 
+      state: status,
       idPeople: infoUserLog.idPeople
     }
     dispatch(putUserData(newStatus))
@@ -29,10 +29,10 @@ function ProfileProvider() {
   const handleFindProfession = () => {
     if (infoUserLog.categories && infoUserLog.categories.length != 0) {
       const profession = infoUserLog.categories.find(category => category.idCategorie === 5);
-      
+
       if (profession && profession.categories_options.length != 0) {
         return profession.categories_options[0].description
-      }      
+      }
     }
   }
 
@@ -41,7 +41,7 @@ function ProfileProvider() {
       <div className={style.wrapper}>
         <div className={style.alertWrapper}>
           {
-            infoUserLog.state === 'Unverified' &&
+            infoUserLog.state === 'Unverified' && infoUserLog.typeOfPerson === 'provider' &&
             <div className={style.verificationAlert}>
               <p className={style.textAlert}>
                 Completa tu perfil para poder verificar tu cuenta. Debes tener los siguientes campos completos: Información personal, Foto de Perfil y Servicios. <br /> Ten en cuenta
@@ -53,61 +53,64 @@ function ProfileProvider() {
         <div className={style.perfilWrapper}>
           <div className={style.imageWrapper}>
             {
-              infoUserLog.state != "Unverified" ?
-                infoUserLog.state === "Active" 
-                ? <button type="button" onClick={() => handleChangeStatus("Inactive")} className={style.stateActive}>{infoUserLog.state}</button>
-                : <button type="button" onClick={() => handleChangeStatus("Active")} className={style.stateInactive}>{infoUserLog.state}</button>
-              : <></>
+              infoUserLog.state != "Unverified" && infoUserLog.typeOfPerson === 'provider' ?
+                infoUserLog.state === "Active"
+                  ? <button type="button" onClick={() => handleChangeStatus("Inactive")} className={style.stateActive}>{infoUserLog.state}</button>
+                  : <button type="button" onClick={() => handleChangeStatus("Active")} className={style.stateInactive}>{infoUserLog.state}</button>
+                : <></>
             }
-        <img
-          className={style.image}
-          src={infoUserLog.image ? infoUserLog.image : profileImage}
-          alt="Imagen"
-        />
-        <UploadWidget setPublicId={setPublicId} user={infoUserLog.idPeople} />
-        <div className={style.valoration}>
-          {[...Array(Math.floor(Number(infoUserLog.averageRating)))].map((_, index) => (
-              <div key={index} className={style.starIcon}></div>
-            ))}
-          <p className={style.textStar}></p>
-          <p className={style.rating}>
-            {infoUserLog.averageRating} ({infoUserLog.countRating})
-          </p>
+            <img
+              className={style.image}
+              src={infoUserLog.image ? infoUserLog.image : profileImage}
+              alt="Imagen"
+            />
+            <UploadWidget setPublicId={setPublicId} user={infoUserLog.idPeople} />
+            <div className={style.valoration}>
+              {[...Array(Math.floor(Number(infoUserLog.averageRating)))].map((_, index) => (
+                <div key={index} className={style.starIcon}></div>
+              ))}
+              <p className={style.textStar}></p>
+              <p className={style.rating}>
+                {infoUserLog.averageRating} ({infoUserLog.countRating})
+              </p>
+            </div>
+          </div>
+          <div className={style.infoWrapper}>
+            <div className={style.nameWrapper}>
+              <p className={style.textName}>{infoUserLog.fullName}</p>
+              {
+                infoUserLog.typeOfPerson === 'provider' &&
+                <div className={infoUserLog.state === "Unverified" ? style.iconNotVerified : style.iconVerified}></div>
+              }
+              {
+                infoUserLog.typeOfPerson === 'provider'
+                  ? infoUserLog.state === "Unverified"
+                    ? <p className={style.textNotVerified}>Cuenta No Verificada</p>
+                    : <p className={style.textVerified}>Cuenta Verificada</p>
+                  : <></>
+              }
+              <button
+                onClick={() => handleShowForm()}
+                className={style.editButton}
+              ></button>
+            </div>
+            <p className={style.textData}>
+              {infoUserLog.age} años | {infoUserLog.address}, {infoUserLog.locationName}, {infoUserLog.provinceName}, {infoUserLog.country}
+            </p>
+            <p className={style.textOcupation}>{handleFindProfession()}</p>
+            <p className={style.textDetail}>{infoUserLog.aboutMe}</p>
+            <div className={style.contacts}>
+              <p className={style.textContact}>Contactos: </p>
+              <div className={style.iconEmail}></div>
+              <p className={style.textEmail}>{infoUserLog.email}</p>
+              <div className={style.iconPhone}></div>
+              <p className={style.textPhone}>{infoUserLog.phone}</p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className={style.infoWrapper}>
-        <div className={style.nameWrapper}>
-          <p className={style.textName}>{infoUserLog.fullName}</p>
-          <div
-            className={infoUserLog.state === "Unverified" ? style.iconNotVerified : style.iconVerified}
-          ></div>
-          {
-            infoUserLog.state === "Unverified"
-              ? <p className={style.textNotVerified}>Cuenta No Verificada</p>
-              : <p className={style.textVerified}>Cuenta Verificada</p>
-          }
-          <button
-            onClick={() => handleShowForm()}
-            className={style.editButton}
-          ></button>
-        </div>
-        <p className={style.textData}>
-          {infoUserLog.age} años | {infoUserLog.address}, {infoUserLog.locationName}, {infoUserLog.provinceName}, {infoUserLog.country}
-        </p>
-        <p className={style.textOcupation}>{handleFindProfession()}</p>
-        <p className={style.textDetail}>{infoUserLog.aboutMe}</p>
-        <div className={style.contacts}>
-          <p className={style.textContact}>Contactos: </p>
-          <div className={style.iconEmail}></div>
-          <p className={style.textEmail}>{infoUserLog.email}</p>
-          <div className={style.iconPhone}></div>
-          <p className={style.textPhone}>{infoUserLog.phone}</p>
-        </div>
-      </div>
-    </div>
-    </div >
-    { showForm && <Form handleShowForm={handleShowForm} />
-}
+      </div >
+      {showForm && <Form handleShowForm={handleShowForm} />
+      }
     </div >
   );
 }
