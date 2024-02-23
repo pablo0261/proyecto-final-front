368b3f5fdba5328ff63ab8fbdafbc3a4e4ea79d7
nitidoart <113176158+nitidoart@users.noreply.github.com>
2024-02-23 03:11:36 +0100

    Modified(SlideCustomer.jsx):Videos tutorial para clientes

--- src/components/Slide/SlideCustomer.jsx
+++ src/components/Slide/SlideCustomer.jsx
@@ -1,15 +1,14 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './SlideCustomer.module.scss';
 import { useInView } from 'react-intersection-observer';
-import video1 from '../../assets/Media/client-01.mp4';
 
 const SlideCustomer = () => {
-  const videoRef = useRef(null);//*
 
-  const [isVideoExpanded, setIsVideoExpanded] = useState(false);//*
+  const [isVideoExpanded, setIsVideoExpanded] = useState(null);
+
   // Función para alternar el tamaño del video
-  const toggleVideoSize = () => {
-    setIsVideoExpanded(prevState => !prevState);
+  const toggleVideoSize = (index) => {
+    setIsVideoExpanded(prevIndex => prevIndex === index ? null : index);
   };
   // Establecer la clase CSS para el video según el estado de expansión
   const videoClassName = isVideoExpanded ? styles.videoExpanded : styles.videoNormal;
@@ -21,21 +20,11 @@ const SlideCustomer = () => {
   const { ref: step5, inView: step5Visible } = useInView();
   const { ref: step6, inView: step6Visible } = useInView();
 
-  useEffect(() => {
-    // Si la sección del video está visible, reproducir automáticamente el video
-    if (step1Visible) {
-      videoRef.current.play();
-    } else {
-      // Pausar el video si la sección no está visible
-      videoRef.current.pause();
-    }
-  }, [step1Visible]);
-
   return (
     <div className={styles.container}>
 
       <section className={`${styles.section} ${step1Visible ? styles.sectionVisible : styles.sectionInVisible}`}>
-      
+
         <div className={styles.numberStep}>1</div>
         <div className={styles.textBox}>
           <p className={styles.textTitle}>Registrate</p>
@@ -43,10 +32,11 @@ const SlideCustomer = () => {
             Por ser tu primera vez deberas ir al boton "Registrarse" y llenar el formulario con tus datos personales.</p>
         </div>
         <div className={styles.imgTutorial}>
-        <video width="500" ref={videoRef} autoPlay loop
-        onClick={toggleVideoSize}
-        className={videoClassName}>
-            <source src={video1} type="video/mp4" />
+          <video width="500" autoPlay loop muted
+            onClick={() => toggleVideoSize(1)}
+            className={isVideoExpanded === 1 ? styles.videoExpanded : styles.videoNormal}
+          >
+            <source src='https://res.cloudinary.com/dn3kedyer/video/upload/v1708651809/client-01.mp4' type="video/mp4" />
           </video>
         </div>
         <div ref={step1} className={styles.pivot}></div>
@@ -60,10 +50,11 @@ const SlideCustomer = () => {
             poder verificar tu cuenta. Esto ayuda a que nuestros cuidadores conozcan mas a sus clientes.</p>
         </div>
         <div className={styles.imgTutorial}>
-        <video width="500" ref={videoRef} autoPlay loop
-        onClick={toggleVideoSize}
-        className={videoClassName}>
-            <source src={video1} type="video/mp4" />
+          <video width="500" autoPlay loop muted
+            onClick={() => toggleVideoSize(2)}
+            className={isVideoExpanded === 2 ? styles.videoExpanded : styles.videoNormal}
+          >
+            <source src='https://res.cloudinary.com/dn3kedyer/video/upload/v1708653984/client-02.mp4' type="video/mp4" />
           </video>
         </div>
         <div ref={step2} className={styles.pivot}></div>
@@ -77,10 +68,11 @@ const SlideCustomer = () => {
             Elige el cuidador que se adecue a tus necesidades e investiga su perfil.</p>
         </div>
         <div className={styles.imgTutorial}>
-        <video width="500" ref={videoRef} autoPlay loop
-        onClick={toggleVideoSize}
-        className={videoClassName}>
-            <source src={video1} type="video/mp4" />
+          <video width="500" autoPlay loop muted
+            onClick={() => toggleVideoSize(3)}
+            className={isVideoExpanded === 3 ? styles.videoExpanded : styles.videoNormal}
+          >
+            <source src='https://res.cloudinary.com/dn3kedyer/video/upload/v1708653993/client-03.mp4' type="video/mp4" />
           </video>
         </div>
         <div ref={step3} className={styles.pivot}></div>
@@ -94,10 +86,11 @@ const SlideCustomer = () => {
             el formulario de peticion y se creará una "Oportunidad" en estado "Pendiente" en la pestaña de "Mis Conexiones"</p>
         </div>
         <div className={styles.imgTutorial}>
-        <video width="500" ref={videoRef} autoPlay loop
-        onClick={toggleVideoSize}
-        className={videoClassName}>
-            <source src={video1} type="video/mp4" />
+          <video width="500" autoPlay loop muted
+            onClick={() => toggleVideoSize(4)}
+            className={isVideoExpanded === 4 ? styles.videoExpanded : styles.videoNormal}
+          >
+            <source src='https://res.cloudinary.com/dn3kedyer/video/upload/v1708651813/client-04.mp4' type="video/mp4" />
           </video>
         </div>
         <div ref={step4} className={styles.pivot}></div>
@@ -111,10 +104,11 @@ const SlideCustomer = () => {
             en la pestaña de "Mis Conexiones" en el estado "Confirmados". Conversa con tu cuidador y concluye los detalles de tus necesidades.</p>
         </div>
         <div className={styles.imgTutorial}>
-        <video width="500" ref={videoRef} autoPlay loop
-        onClick={toggleVideoSize}
-        className={videoClassName}>
-            <source src={video1} type="video/mp4" />
+          <video width="500" autoPlay loop muted
+            onClick={() => toggleVideoSize(5)}
+            className={isVideoExpanded === 5 ? styles.videoExpanded : styles.videoNormal}
+          >
+            <source src='https://res.cloudinary.com/dn3kedyer/video/upload/v1708651809/client-05.mp4' type="video/mp4" />
           </video>
         </div>
         <div ref={step5} className={styles.pivot}></div>
@@ -128,10 +122,11 @@ const SlideCustomer = () => {
             conocer la calidad del cuidador. No olvides completar este paso porque nos ayuda mejorar la calidad de la oferta de proveedores.</p>
         </div>
         <div className={styles.imgTutorial}>
-        <video width="500" ref={videoRef} autoPlay loop
-        onClick={toggleVideoSize}
-        className={videoClassName}>
-            <source src={video1} type="video/mp4" />
+          <video width="500" autoPlay loop muted
+            onClick={() => toggleVideoSize(6)}
+            className={isVideoExpanded === 6 ? styles.videoExpanded : styles.videoNormal}
+          >
+            <source src='https://res.cloudinary.com/dn3kedyer/video/upload/v1708651809/client-06.mp4' type="video/mp4" />
           </video>
         </div>
         <div ref={step6} className={styles.pivot}></div>
